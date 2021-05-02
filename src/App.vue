3cfc71618498624ef74a5e5b5268f0a62076b8d7
Lawrance Massanja <massanjal4@gmail.com>
2021-05-02 16:13:11 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header/>
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+
+</template>
+<script>
+
+import Header from '@/components/Header';
+
+
+export default {
+  components: {
+    Header
+  }
+}
+
+</script>
